36de20846fdd291d91004ef3396b06d31f4256a6
gl <g.linders@ame.nu>
2021-11-08 14:53:21 +0100

    Adjust shell scripts, ignore bin/hexfiles in dist folder

--- dist/flash.sh
+++ dist/flash.sh
@@ -1,11 +1,10 @@
-./bin/openocd-efm32s2 -s scripts -f interface/cmsis-dap.cfg \
-	-f target/efm32s2.cfg\
+./bin/openocd-efm32s2 -s scripts -f interface/ft423h-module-swd.cfg \
+	-f target/efm32s2.cfg \
 	-c 'transport select swd' \
 	-c init \
+	-c reset \
 	-c halt \
-	-c 'flash probe 0' \
-	-c 'flash banks' \
-	-c 'flash list' \
 	-c 'flash write_image erase '$1 \
-	-c exit\
+	-c 'flash verify_image '$1 \
+	-c exit \
 
